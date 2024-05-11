1e005c3216d340b1cfd07c717d88599f18604488
Sandeep <sandygupta695@gmail.com>
2024-05-11 17:12:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/snakegameusingjetpackcompose/ui/theme/SnakeShapes.kt
@@ -0,0 +1,13 @@
+// SnakeShapes.kt
+
+package com.example.snakegameusingjetpackcompose.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val SnakeShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
